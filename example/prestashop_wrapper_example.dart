12c3f828c390128ce5b978d4f52e37556717c484
Antonio Caballero <acaballeroencinas@gmail.com>
2022-10-29 13:30:52 +0200

    Initial commit

---
+++ example/prestashop_wrapper_example.dart
@@ -0,0 +1,6 @@
+import 'package:prestashop_wrapper/prestashop_wrapper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/prestashop_wrapper.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library prestashop_wrapper;
+
+export 'src/prestashop_wrapper_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
